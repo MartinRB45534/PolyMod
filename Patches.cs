9eb36abe672ba630f2b42fd932f315930bf0bda3
HighFlyer222 <hflyer222@protonmail.com>
2023-11-19 21:58:56 +0100

    Add unused compact UI setting back

--- Patches.cs
+++ Patches.cs
@@ -51,7 +51,7 @@ namespace PolyMod
 		}
 
 		[HarmonyPatch(typeof(SettingsScreen), nameof(SettingsScreen.CreateLanguageList))]
-		private static bool SettingsScreen_CreateLanguageList_(SettingsScreen __instance, UnityEngine.Transform parent)
+		private static bool SettingsScreen_CreateLanguageList(SettingsScreen __instance, UnityEngine.Transform parent)
 		{
 			List<string> list = new() { "Automatic", "English", "Français", "Deutsch", "Italiano", "Português", "Русский", "Español", "日本語", "한국어" };
 			List<int> list2 = new() { 1, 3, 7, 9, 6, 4, 5, 8, 11, 12 };
@@ -73,5 +73,32 @@ namespace PolyMod
 
 			return false;
 		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(SettingsToggleGroup), nameof(SettingsToggleGroup.SetupContainers))]
+		private static void SettingsToggleGroup_SetupContainers(SettingsToggleGroup __instance)
+		{
+			__instance.compactUIContainer.gameObject.SetActive(true);
+			__instance.compactUIContainer.Value = SettingsUtils.UseCompactUI;
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(SettingsUtils), nameof(SettingsUtils.UseCompactUI), MethodType.Getter)]
+		private static void SettingsUtils_UseCompactUI(ref bool __result)
+		{
+			__result = PlayerPrefsUtils.GetBoolValue("useCompactUI", false);
+		}
+
+		// This makes tribe selection popups fullscreen like on mobile if compact UI is on, not sure if we should keep it but it would be funny
+		
+		// [HarmonyPostfix]
+		// [HarmonyPatch(typeof(SelectTribePopup), nameof(SelectTribePopup.OnEnable))]
+		// private static void SelectTribePopup_OnEnable(ref PopupBase __instance)
+		// {
+		// 	if (SettingsUtils.UseCompactUI)
+		// 	{
+		// 		__instance.fullscreenVariant = true;
+		// 	}
+		// }
 	}
 }
