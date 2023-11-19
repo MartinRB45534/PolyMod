69bade569d0f5ffeb0a3b6e777aaffdb862f9546
HighFlyer222 <hflyer222@protonmail.com>
2023-11-19 16:23:42 +0100

    Add the custom language option back (in possibly the worst way...)

--- Patches.cs
+++ Patches.cs
@@ -49,5 +49,29 @@ namespace PolyMod
 		{
 			Commands.Help();
 		}
+
+		[HarmonyPatch(typeof(SettingsScreen), nameof(SettingsScreen.CreateLanguageList))]
+		private static bool SettingsScreen_CreateLanguageList_(SettingsScreen __instance, UnityEngine.Transform parent)
+		{
+			List<string> list = new() { "Automatic", "English", "Français", "Deutsch", "Italiano", "Português", "Русский", "Español", "日本語", "한국어" };
+			List<int> list2 = new() { 1, 3, 7, 9, 6, 4, 5, 8, 11, 12 };
+			if (GameManager.GetPurchaseManager().IsTribeUnlocked(Polytopia.Data.TribeData.Type.Elyrion))
+			{
+				list.Add("∑∫ỹriȱŋ");
+				list2.Add(10);
+			}
+			list.Add("Custom...");
+			list2.Add(2);
+			__instance.languageSelector = UnityEngine.Object.Instantiate(__instance.horizontalListPrefab, parent ?? __instance.container);
+			__instance.languageSelector.UpdateScrollerOnHighlight = true;
+			__instance.languageSelector.HeaderKey = "settings.language";
+			__instance.languageSelector.SetIds(list2.ToArray());
+			__instance.languageSelector.SetData(list.ToArray(), 0, false);
+			__instance.languageSelector.SelectId(SettingsUtils.Language, true, -1f);
+			__instance.languageSelector.IndexSelectedCallback = new Action<int>(__instance.LanguageChangedCallback);
+			__instance.totalHeight += __instance.languageSelector.rectTransform.sizeDelta.y;
+
+			return false;
+		}
 	}
 }
