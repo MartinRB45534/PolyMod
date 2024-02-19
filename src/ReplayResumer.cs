84ee15d77afb17784a275b38188d7913b072fd80
Martin <martin.rialhe-badet@etu.univ-smb.fr>
2024-02-19 21:29:55 +0100

    It partly works, I don't understand how though

--- src/ReplayResumer.cs
+++ src/ReplayResumer.cs
@@ -9,10 +9,10 @@ namespace PolyMod
         public async void ResumeAsHotseatGame()
         {
             // Check that we are in a replay
-            ReplayClient replayClient = GameManager.Client as ReplayClient;
-            if(replayClient == null)
+            ClientBase replayClient = GameManager.Client;
+            if(!replayClient.IsReplay)
             {
-                Log.Warning("{0} Command used outside of replay game", new Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<Il2CppSystem.Object>(new Il2CppSystem.Object[] { "<color=#FFFFFF>[GameManager]</color>" }));
+                Log.Warning("{0} Command used outside of replay game, client is {1}", new Il2CppSystem.Object[] { "<color=#FFFFFF>[GameManager]</color>", GameManager.Client.GetType().ToString()});
                 GameManager.instance.SetLoadingGame(false);
                 return;
             }
@@ -24,32 +24,45 @@ namespace PolyMod
                 "<color=#FFFFFF>[GameManager]</color>",
                 GameManager.instance.settings.BaseGameMode.ToString()
             });
-            GameManager.instance.SetHotseatClient();
             
-            // Get the client as a hotseat client
-            HotseatClient hotseatClient = GameManager.Client as HotseatClient;
+            // Get the new hotseat client
+            HotseatClient hotseatClient = SetHotseatClient();
             if(hotseatClient == null)
             {
-                Log.Warning("{0} Failed to create Hotseat game", new Il2CppInterop.Runtime.InteropTypes.Arrays.Il2CppReferenceArray<Il2CppSystem.Object>(new Il2CppSystem.Object[] { "<color=#FFFFFF>[GameManager]</color>" }));
+                Log.Warning("{0} Failed to create Hotseat game", new Il2CppSystem.Object[] { "<color=#FFFFFF>[GameManager]</color>" });
                 GameManager.instance.SetLoadingGame(false);
                 return;
             }
             TaskAwaiter<bool> taskAwaiter = TransformClient(replayClient, hotseatClient).GetAwaiter();
-            if (!taskAwaiter.IsCompleted)
-            {
-                await taskAwaiter;
-                TaskAwaiter<bool> taskAwaiter2;
-                taskAwaiter = taskAwaiter2;
-                taskAwaiter2 = default(TaskAwaiter<bool>);
-            }
             if (taskAwaiter.GetResult())
             {
                 GameManager.instance.LoadLevel();
+                MakeResumePopup();
             }
         }
 
-        public Il2CppSystem.Threading.Tasks.Task<bool> TransformClient(ReplayClient replayClient, HotseatClient hotseatClient)
+        public HotseatClient SetHotseatClient()
         {
+            GameManager.instance.settings.GameType = GameType.PassAndPlay;
+            Log.Verbose("{0} Setting up hotseat client...", new Il2CppSystem.Object[] { "<color=#FFFFFF>[GameManager]</color>"});
+            HotseatClient hotseatClient = new HotseatClient
+            {
+                OnConnected = new Action(GameManager.instance.OnLocalClientConnected),
+                OnDisconnected = new Action(GameManager.instance.OnClientDisconnected),
+                OnSessionOpened = new Action(GameManager.instance.OnClientSessionOpened),
+                OnStateUpdated = new Action<StateUpdateReason>(GameManager.instance.OnStateUpdated),
+                OnStartedProcessingActions = new Action(GameManager.instance.OnStartedProcessingActions),
+                OnFinishedProcessingActions = new Action(GameManager.instance.OnFinishedProcessingActions)
+            };
+            GameManager.instance.client = hotseatClient;
+            return hotseatClient;
+        }
+
+        public Il2CppSystem.Threading.Tasks.Task<bool> TransformClient(ClientBase replayClient, HotseatClient hotseatClient)
+        {
+            // Modify the gameState as needed
+            replayClient.initialGameState.Settings.GameType = GameType.PassAndPlay;
+            replayClient.currentGameState.Settings.GameType = GameType.PassAndPlay;
             // Mimic the session opening process
             Log.Verbose("{0} Transforming replay session...", new Il2CppSystem.Object[]
                 {HotseatClient.LOG_PREFIX,
@@ -66,7 +79,21 @@ namespace PolyMod
             hotseatClient.UpdateGameStateImmediate(hotseatClient.currentGameState, StateUpdateReason.GameJoined);
             hotseatClient.SaveSession(hotseatClient.gameId, false);
             hotseatClient.PrepareSession();
-            return Task.FromResult<bool>(true);            
+            return Il2CppSystem.Threading.Tasks.Task.FromResult<bool>(true);            
+        }
+
+        public void MakeResumePopup()
+        {
+            // Make a popup to tell the user that the game is resuming from replay
+            BasicPopup popup = PopupManager.GetBasicPopup(new PopupManager.BasicPopupData{
+                header = "Resuming from Replay",
+                description = "The game has been turned from a replay into a hotseat game. You can now continue playing.",
+                buttonData = new PopupBase.PopupButtonData[]
+                {
+                    new PopupBase.PopupButtonData("buttons.ok", PopupBase.PopupButtonData.States.Selected, null, -1, true, null)
+                }
+            });
+            popup.Show();
         }
     }
 }
